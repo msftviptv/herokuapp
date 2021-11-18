be2a4291297d38fb3e575f551bc83d121fe559d3
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-11-18 13:49:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,88 @@
-<?php
 
-echo"hellow";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<!--================== Design Provide By HeNy007 ==================-->
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="copyright" content="HeNy007">
+<meta property="og:url" content="https://aliwixplay.netlify.app/" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="تطبيق أليويكس بلاي ‫-‬ Aliwix Play" />
+<meta property="og:description" content="أليويكس تي في - التطبيق التونسي المجاني الأول لمشاهدة المئات من القنوات التلفزيونية الفضائية والمشفرة بالإضافة لمكتبة منوعة من الأفلام و المسلسلات التونسية و العالمية العربية والأجنبية المترجمة بجودة عالية وبدون تقطيع بواسطة هاتفك الذكي أو باستخدام جهازك اللوحي أو " التلفزيون الذكي" مجاناً Aliwix Play, Aliwix Play apk, تحميل تطبيق أليويكس بلاي">
+<meta name="keywords" content="apk,Aliwix Play,Aliwix,tv,تطبيق أليويكس بلاي،أليويكس تي في،تحميل أليويكس بلاي">
+<meta property="og:image" content="http://Aliwix Playps.com/app/assets/img/mockup.png" />
+<title>Aliwix Password</title>
+<link rel="shortcut icon" href="https://github.com/HeNy007/AliwixTV/raw/main/20210619_180526.png" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- CSS Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<style>
+body
+{
+  background:black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+#typing {
+  
+			width: 0ch;
+			color: white;
+			float: left;
+			font-family: sans-serif;
+			font-weight:bold;
+			font-size: 33px;
+			overflow: hidden;
+			white-space: nowrap;
+			animation: typing 5s steps(12) 1s infinite alternate;
+		}
+        #typing span{
+        color:red;}
+
+		#HeNy {
+			
+      
+      color: yellow;
+			
+			font-weight:bold;
+			font-size: 35px;
+			}
+
+
+		@keyframes typing {
+			from {
+				width: 0ch;
+			}
+			to {
+				width: 22ch;
+			}
+		}
+		@keyframes crow {
+			from {
+				opacity: 0;
+			}
+			to {
+				opacity: 1;
+			}
+		}
+</style>
+</head>
+<body>
+<div id="typing"> Password = <span>  Aliwix212 </span></div>
+<br>
+<br> 
+<br>
+<br> 
+<div id="HeNy">
+<p style="text-align: center;">&nbsp;👇👇</p><p style="text-align: center;"><span style="color: #fcff01;">Aliwix212</span></p>
+</div>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
