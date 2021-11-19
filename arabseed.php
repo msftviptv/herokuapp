7804bf9a59add21ad296a21b2b26abecb1b1ba93
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-11-19 14:50:28 +0200

    Create arabseed.php

---
+++ arabseed.php
@@ -0,0 +1,11 @@
+<?php
+$url ="https://m.arabseed.me/embed-".$_GET['s'].".html";
+$content = file_get_contents($url);
+$first_step = explode( 'source src="' , $content );
+$second_step = explode('" type="video' , $first_step[1] );
+
+$user= $second_step[0];
+
+$link = $user;
+header('Location: '.$link);
+?>
