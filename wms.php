e7599a0b67ae0a557b9ab522fa796bd81a933479
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-11-20 10:43:06 +0200

    Update wms.php

--- wms.php
+++ wms.php
@@ -5,7 +5,7 @@ $opts = array(
 'header'=>"Referer: https://daddylive.me/embed/stream-91.php" .
   "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0"
 ));
-$url = file_get_contents('https://jazzy.to/daddylive.php?live=91&vw=100vw&vh=100vh', false, stream_context_create($opts));
+$url = file_get_contents('https://jazzy.to/daddylive.php?live='.$_GET['s'].'&vw=100vw&vh=100vh', false, stream_context_create($opts));
 $first_step = explode( 'source: "' , $url );
 $second_step = explode('",' , $first_step[1] );
 
