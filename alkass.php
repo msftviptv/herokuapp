bd604fb1dc96a31a01721a9207421cac8c0e7117
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-11-20 11:24:44 +0200

    Create alkass.php

---
+++ alkass.php
@@ -0,0 +1,12 @@
+<?php
+$url ="https://www.alkass.net/alkass/live.aspx?ch=".$_GET['ch'];
+$content = file_get_contents($url);
+$first_step = explode( 'source: " ' , $content );
+$second_step = explode('",' , $first_step[1] );
+
+$user= $second_step[0];
+
+$link = $user;
+echo $link;
+//header('Location: '.$link);
+?>
