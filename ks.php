b5e0a4f3c0f614bb48dce49fa4ba715eea95fcbc
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-11-20 10:23:05 +0200

    update ks.php

--- ks.php
+++ ks.php
@@ -1,11 +1,11 @@
 <?php
-$url ='https://anacondaapp.herokuapp.com/kc.php';
+$url ='https://anacondaapp.herokuapp.com/egy.php';
 $content = file_get_contents($url);
-$first_step = explode( 's9543724_url=' , $content );
-$second_step = explode("450.ts" , $first_step[1] );
+$first_step = explode( 's1048249_url=' , $content );
+$second_step = explode("218]" , $first_step[1] );
 
 $user= $second_step[0];
 
 $link = $user.$_GET['s'];
-header('Location: '.$link.'.ts');
+header('Location: '.$link);
 ?>
