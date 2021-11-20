930047a82d16dd999997a096cc2dd2b5ac172339
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-11-20 10:52:50 +0200

    Update wms.php

--- wms.php
+++ wms.php
@@ -10,6 +10,12 @@ $first_step = explode( 'source: "' , $url );
 $second_step = explode('",' , $first_step[1] );
 
 $user= $second_step[0];
-
-echo $user;
+$optc = array(
+'http'=>array(
+'method'=>"GET",
+'header'=>"Referer: https://jazzy.to/" .
+  "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0"
+));
+$uri = file_get_contents($user, false, stream_context_create($optc));
+header('Location: '.$uri);
 ?>
