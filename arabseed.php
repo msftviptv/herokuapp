adfff0788c09f0c68f7d5a513cbea69b7bdcb422
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-11-20 10:30:11 +0200

    Update arabseed.php

--- arabseed.php
+++ arabseed.php
@@ -1,5 +1,5 @@
 <?php
-$url ="https://m.arabseed.me/embed-".$_GET['s'].".html";
+$url ="https://m.arabseed.me/embed-".$_GET['vid'].".html";
 $content = file_get_contents($url);
 $first_step = explode( 'source src="' , $content );
 $second_step = explode('" type="video' , $first_step[1] );
