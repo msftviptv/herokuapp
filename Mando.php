0e7fb87b574412be9567e7c471798284dffbd8f5
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-12-01 21:27:49 +0200

    update Mando.php

--- Mando.php
+++ Mando.php
@@ -1,6 +1,6 @@
 <?php
 $ch  = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://config.e-droid.net/srv/config.php?v=90&vname=1&idapp=904969&idusu=1641138491&cod_g=2m2pfrp8&pw=1&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=ad.rar.tv&fus=051021103531&aid=40739dc111358653');
+curl_setopt($ch, CURLOPT_URL, 'https://config.e-droid.net/srv/config.php?v=128&vname=9.8&idapp=1765693&idusu=0&cod_g=&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=mando.sport1&fus=010100000000&aid=40739dc111358653');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Android Vinebre Software');
 $html = curl_exec($ch);
