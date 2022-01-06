4412d394eaa06ac78077de5398ae1486b94d72a6
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-01-06 18:17:30 +0200

    update kc.php

--- kc.php
+++ kc.php
@@ -1,6 +1,6 @@
 <?php
 $ch  = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://config.e-droid.net/srv/config.php?v=128&vname=9.8&idapp=1765693&idusu=0&cod_g=&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=mando.sport1&fus=010100000000&aid=40739dc111358653');
+curl_setopt($ch, CURLOPT_URL, 'https://config.e-droid.net/srv/config.php?v=128&vname=9.8&idapp=840321&idusu=0&cod_g=&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=alkaicer.com&fus=010100000000&aid=40739dc111358653');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Android Vinebre Software');
 $html = curl_exec($ch);
