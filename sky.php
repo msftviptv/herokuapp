ac56762bc5507d86dc3872d37c0d7c6c7f06a3ff
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-01-12 13:13:46 +0200

    update sky.php

--- sky.php
+++ sky.php
@@ -1,6 +1,6 @@
 <?php
 $ch  = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://config.e-droid.net:80/srv/config.php?v=24&vname=1.0&idapp=1238418&idusu=0&cod_g=&pw=1&gp=0&am=0&pa_env=1&pa=FR&pn=skgroup.app&fus=010100000000&recup_todo=1&aid=4e3a984ec951910');
+curl_setopt($ch, CURLOPT_URL, 'https://config.e-droid.net/srv/config.php?v=128&vname=9.8&idapp=1238418&idusu=0&cod_g=&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=skgroup.app&fus=010100000000&aid=40739dc111358653');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Android Vinebre Software');
 $html = curl_exec($ch);
