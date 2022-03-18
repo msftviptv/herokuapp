51f61d8258d4e2eb64c29f64a61b03c929dfdc25
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-03-18 09:17:19 +0200

    Create karwan.php

---
+++ karwan.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-type: application/text");
+header("Content-Disposition: attachment; filename=index.m3u8");
+$url ="https://ku.karwan.tv/live/".$_GET['s'].".php";
+$content = file_get_contents($url);
+$first_step = explode( 'source src="' , $content );
+$second_step = explode('" type="application' , $first_step[1] );
+
+$user= $second_step[0];
+
+$link = $user;
+header('Location: '.$link);
+?>
