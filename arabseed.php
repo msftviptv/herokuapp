933179314a4f9f779cbdaf395a38797109e7f7fb
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-03-18 09:44:40 +0200

    Update arabseed.php

--- arabseed.php
+++ arabseed.php
@@ -1,6 +1,6 @@
 <?php
-header("Content-type: application/text");
-header("Content-Disposition: attachment; filename=index.m3u8");
+//header("Content-type: application/text");
+//header("Content-Disposition: attachment; filename=index.m3u8");
 $url ="https://m.arabseed.me/embed-".$_GET['s'].".html";
 $content = file_get_contents($url);
 $first_step = explode( 'source src="' , $content );
@@ -9,5 +9,6 @@ $second_step = explode('" type="video' , $first_step[1] );
 $user= $second_step[0];
 
 $link = $user;
-header('Location: '.$link);
+//header('Location: '.$link);
+echo $link;
 ?>
