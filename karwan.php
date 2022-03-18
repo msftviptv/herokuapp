09b3e220c694a5097b74e1cb8596c3eaadf2b92b
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-03-18 15:30:29 +0200

    Update karwan.php

--- karwan.php
+++ karwan.php
@@ -1,14 +1,28 @@
 <?php
-//header("Content-type: application/text");
-//header("Content-Disposition: attachment; filename=index.m3u8");
-$url ="https://ku.karwan.tv/live/mmn-sport.php";
-$content = file_get_contents($url);
-$first_step = explode( 'source src="' , $content );
+header("Content-type: application/text");
+header("Content-Disposition: attachment; filename=index.m3u8");
+
+
+
+$url ="https://ku.karwan.tv/live/".$_GET['ch'].".php";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);  
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 3);     
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+$html = curl_exec($ch);
+curl_close($ch);
+$first_step = explode( 'source src="' , $html );
 $second_step = explode('" type="application' , $first_step[1] );
 
 $user= $second_step[0];
 
 $link = $user;
 //header('Location: '.$link);
+
 echo $link;
 ?>
