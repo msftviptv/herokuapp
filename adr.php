6589acf391653314b945f740d07b31353d9d3bb4
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-04-13 21:55:03 +0200

    update adr.php

--- adr.php
+++ adr.php
@@ -1,6 +1,6 @@
 <?php
 $ch  = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://config.e-droid.net/srv/config.php?v=90&vname=1&idapp=904969&idusu=1641138491&cod_g=2m2pfrp8&pw=1&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=ad.rar.tv&fus=030422120219&aid=40739dc111358653');
+curl_setopt($ch, CURLOPT_URL, 'https://config.e-droid.net/srv/config.php?v=90&vname=1&idapp=1980278&idusu=1641138491&cod_g=2m2pfrp8&pw=1&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=com.elnmr.tv&fus=030422120219&aid=40739dc111358653');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Android Vinebre Software');
 $html = curl_exec($ch);
