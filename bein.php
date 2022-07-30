6a5288f4dc39221c96fb5b2107c373841b81a51b
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-07-30 16:47:36 +0200

    Update bein.php

--- bein.php
+++ bein.php
@@ -1,8 +1,8 @@
 <?php
-$url ='http://grgs2020.000webhostapp.com/ar.php';
+$url ='http://assia23.com/live/arena-sport/';
 $content = file_get_contents($url);
-$first_step = explode( 'enableVideo("' , $content );
-$second_step = explode('");' , $first_step[1] );
+$first_step = explode( "source: '" , $content );
+$second_step = explode("'," , $first_step[1] );
 
 $user= $second_step[0];
 
