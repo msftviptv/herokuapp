d010ff79835545c90d499bba2c469cee1e9b9691
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-08-15 13:31:41 +0200

    Create nginx.php

---
+++ nginx.php
@@ -0,0 +1,115 @@
+
+<html style="margin:0;padding:0;">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex">
+	<title></title>
+	<style type="text/css">
+		padding-top: 0;
+		padding-bottom: 0;
+		padding-left: 0;
+		padding-right: 0;
+	</style>
+
+
+
+</head>
+<body style="margin:0;padding:0;position: absolute;left: 0;right: 0;top: 0;bottom: 0;background: #000;">
+	
+		
+		
+		
+			
+			
+			
+<div id="prl" style="position: absolute;left: 0;right: 0;bottom: 0;top: 0;background: #000;"><style>.lds-ripple {display: inline-block;position: relative;width: 80px;height: 80px;}.lds-ripple div {position: absolute;border: 4px solid #fff;opacity: 1;border-radius: 50%;animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;}.lds-ripple div:nth-child(2) {animation-delay: -0.5s;}@keyframes lds-ripple{0%{top: 36px;left: 36px;width: 0;height: 0;opacity: 0;}4.9%{top: 36px;left: 36px;width: 0;height: 0;opacity: 0;}5%{top: 36px;left: 36px;width: 0;height: 0;opacity: 1;}100%{top: 0px;left: 0px;width: 72px;height: 72px;opacity: 0;}}</style><div class="lds-ripple" style="left: 50%;top: 50%;transform: translate(-50%,-50%)"><div></div><div></div></div></div>
+
+<script type="text/javascript">
+	const key = '18ab48f94b520db7f8f24034f1666862';
+	var get_str = function( k, u, callback ){
+        var xmlhttp = new XMLHttpRequest( );
+        xmlhttp.onreadystatechange = function() {
+            if( xmlhttp.readyState == XMLHttpRequest.DONE ){   
+                // XMLHttpRequest.DONE == 4
+                if( xmlhttp.status == 200 ){
+                   callback( xmlhttp.responseText );
+                }
+            }
+        };
+        xmlhttp.open( 'GET', '/access2.php?key=' + k + '&u=' + u + '&l=ls', true ); xmlhttp.send( );
+    }
+
+    //get_str( key, '92', function( res ){
+
+    //	res = JSON.parse( res );
+
+    	var id = '92';
+    	var res = [ 'https://cdn.sportcast.life/embed10/live' + id + '.php', 'https://besthdplayer.click/premiumtv/livetvon.php?id=' + id ];
+
+    	//var res = [ 'https://cdn.sportcast.life/embed3/live' + id + '.php', 'https://cdn.sportcast.life/embed7/live' + id + '.php' ];
+
+    	//var res = [ 'https://daddylive.one/stream/stream-' + id + '.php', 'https://besthdplayer.click/premiumtv/daddylive.php?id=' + id ];
+
+    	//var res = [ 'https://daddylive.futbol/live/stream-' + id + '.php', 'https://rkc.primetubsub.xyz/premiumtv/daddylive.php?id=' + id ];
+
+
+
+    	
+    	
+
+    	//var res = [ 'https://cdn.sportcast.life/embed3/live' + id + '.php', 'https://cdn.sportcast.life/embed10/live' + id + '.php' ];
+
+    	//var res = [ 'https://cdn.sportcast.life/embed3/live' + id + '.php', 'https://cdn.sportcast.life/embed42/live' + id + '.php' ];
+
+    	//var res = [ 'https://daddylive.one/cast/stream-' + id + '.php', 'https://player.licenses4.me/player.php?id=premium' + id + '' ];
+
+    	var preloader = '<div id="prl" style="position: absolute;left: 0;right: 0;bottom: 0;top: 0;background: #000;"><style>.lds-ripple {display: inline-block;position: relative;width: 80px;height: 80px;}.lds-ripple div {position: absolute;border: 4px solid #fff;opacity: 1;border-radius: 50%;animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;}.lds-ripple div:nth-child(2) {animation-delay: -0.5s;}@keyframes lds-ripple{0%{top: 36px;left: 36px;width: 0;height: 0;opacity: 0;}4.9%{top: 36px;left: 36px;width: 0;height: 0;opacity: 0;}5%{top: 36px;left: 36px;width: 0;height: 0;opacity: 1;}100%{top: 0px;left: 0px;width: 72px;height: 72px;opacity: 0;}}</style><div class="lds-ripple" style="left: 50%;top: 50%;transform: translate(-50%,-50%)"><div></div><div></div></div></div>';
+    	
+    	document.write( '<iframe id="spk" src="' + res[ 0 ] + '" width="0" height="0" style="opacity:0;" sandbox="allow-forms allow-same-origin allow-scripts" referrerpolicy2="no-referrer"></iframe>' + preloader );
+
+    	//document.querySelector( '#spk' ).addEventListener( 'load', (event) => {
+
+			setTimeout( function(){
+				document.querySelector( '#spk' ).remove( );
+		  		document.write( '<style>body{margin:0;padding:0;position: absolute;left: 0;right: 0;top: 0;bottom: 0;background: #000;}</style><iframe id="sl" class="responsive" allow="encrypted-media" width="100%" height="100%" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" allowfullscreen="yes" src="' + res[ 1 ] + '" onerror="this.onerror=null;this.src=\'' + res[ 1 ] + '\'" style="position: absolute;left: 0;right: 0;top: 0;bottom: 0;" referrerpolicy2="no-referrer" sandbox2="allow-forms allow-same-origin allow-scripts"></iframe>' );
+
+		  		document.querySelector( '#sl' ).addEventListener( 'load', (event) => {
+		  			if( document.querySelector( '#prl' ) ) document.querySelector( '#prl' ).remove( );
+		  		});
+
+		  		var script = document.createElement( 'script' );
+				script.setAttribute( 'async', true );
+				script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-97551325-28';
+				document.body.appendChild( script );
+				var script = document.createElement( 'script' );
+				script.src = 'https://cdn.sportcast.life/scripts/ga.js';
+				document.body.appendChild( script );
+
+			}, 2500 );
+		  	
+		//});
+
+		
+
+    //});
+
+</script>
+
+
+
+
+
+		
+		<script type="text/javascript">
+	  		var script = document.createElement( 'script' );
+	  		script.setAttribute( 'async', true );
+	  		script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-97551325-28';
+	  		document.body.appendChild( script );
+	  		var script = document.createElement( 'script' );
+	  		script.src = 'https://cdn.sportcast.life/scripts/ga.js';
+	  		document.body.appendChild( script );
+		</script>			
+
+	<script>(function(){var js = "window['__CF$cv$params']={r:'73b198541fda416c',m:'TPJULOTE2nOjaJlVtyTStKCAXI7c1WFt_ivPi45fHN4-1660562993-0-AWj17kEFHGvl+Q+OCzp6ZdNXpkkzuYjYCiGwfZjUusy/ya0S3ErVHMk+N9/boviU3PqjmhxHw41/Cuh+c6nxTcNmcF+OOa8H/kqoShA2xp8T9szBQRGvDByMoYTjEY+jXQ==',s:[0x513d98a4c5,0xb5bc321d17],u:'/cdn-cgi/challenge-platform/h/b'};var now=Date.now()/1000,offset=14400,ts=''+(Math.floor(now)-Math.floor(now%offset)),_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='/cdn-cgi/challenge-platform/h/b/scripts/alpha/invisible.js?ts='+ts,document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.nonce = '';_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script></body>
+</html>
