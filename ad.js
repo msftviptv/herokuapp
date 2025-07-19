ad11b1af81c3f299fd372af637e6b08a1f2d7317
msftviptv <64074648+msftviptv@users.noreply.github.com>
2025-07-19 08:57:15 +0300

    update ad.js

--- ad.js
+++ ad.js
@@ -1,13 +1,10 @@
-const axios = require('axios');
 
-axios.get('https://config.e-droid.net/srv/config.php?v=197&vname=10.0.1&idapp=2792810&idusu=0&cod_g=&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=app.arnewapp525.con&fus=010100000000&aid=952e48ba83e7e60b', {
+fetch('https://config.e-droid.net/srv/config.php?v=197&vname=10.0.1&idapp=2792810&idusu=0&cod_g=&gp=1&am=0&idl=en&pa_env=1&pa=US&pn=app.arnewapp525.con&fus=010100000000&aid=952e48ba83e7e60b', {
+  method: 'POST',
   headers: {
+    'Content-Type': 'application/json',
     'User-Agent': 'Android Vinebre Software'
-  }
-})
-.then(response => {
-  console.log(response.data);
-})
-.catch(error => {
-  console.error(error);
+  
+  },
+  body: JSON.stringify({ userAgent: navigator.userAgent })
 });
